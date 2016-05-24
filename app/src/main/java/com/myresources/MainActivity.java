6ad8a71170f22249b8d61582e0bee3358e6a8a24
llijingjing <lijingjing1108>
2016-05-24 14:12:20 +0800

    主activity

--- app/src/main/java/com/myresources/MainActivity.java
+++ app/src/main/java/com/myresources/MainActivity.java
@@ -3,6 +3,10 @@ package com.myresources;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+/**
+ * 2016-5-24 14:00
+ * lijingjing
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override

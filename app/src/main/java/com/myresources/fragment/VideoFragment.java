3120fc94c4c97f5fc7ad6a7fe64a902f74efc23c
llijingjing <lijingjing1108>
2016-05-31 14:04:34 +0800

    视频fragment

--- app/src/main/java/com/myresources/fragment/VideoFragment.java
+++ app/src/main/java/com/myresources/fragment/VideoFragment.java
@@ -1,84 +1,32 @@
 package com.myresources.fragment;
 
-import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.myresources.R;
+import com.myresources.VideoActivity;
 
 public class VideoFragment extends Fragment{
+	private View view;
+	private TextView video_tv;
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		Log.d("---222-----",  "onCreateView");
-		return inflater.inflate(R.layout.video_fragment, null);
-	}
-	
-	@Override
-	public void onAttach(Activity activity) {
-		// TODO Auto-generated method stub
-		super.onAttach(activity);
-		Log.d("---222-----",  "onAttach");
-	}
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		// TODO Auto-generated method stub
-		super.onCreate(savedInstanceState);
-		Log.d("---222-----",  "onCreate");
-	}
-	@Override
-	public void onActivityCreated(Bundle savedInstanceState) {
-		// TODO Auto-generated method stub
-		super.onActivityCreated(savedInstanceState);
-		Log.d("---222-----",  "onActivityCreated");
-	}
-	
-	@Override
-	public void onStart() {
-		// TODO Auto-generated method stub
-		super.onStart();
-		Log.d("---222-----",  "onStart");
-	}
-	
-	@Override
-	public void onResume() {
-		// TODO Auto-generated method stub
-		super.onResume();
-		Log.d("---222-----",  "onResume");
-	}
-	
-	@Override
-	public void onPause() {
-		// TODO Auto-generated method stub
-		super.onPause();
-		Log.d("---222-----",  "onPause");
-	}
-	@Override
-	public void onStop() {
-		// TODO Auto-generated method stub
-		super.onStop();
-		Log.d("---222-----",  "onStop");
-	}
-	@Override
-	public void onDestroyView() {
-		// TODO Auto-generated method stub
-		super.onDestroyView();
-		Log.d("---222-----",  "onDestroyView");
-	}
-	
-	@Override
-	public void onDestroy() {
-		// TODO Auto-generated method stub
-		super.onDestroy();
-		Log.d("---222-----",  "onDestroy");
-	}
-	@Override
-	public void onDetach() {
-		// TODO Auto-generated method stub
-		super.onDetach();
-		Log.d("---222-----",  "onDetach");
+		view = inflater.inflate(R.layout.video_fragment, null);
+		video_tv = (TextView) view.findViewById(R.id.video_tv);
+		video_tv.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent(getActivity(), VideoActivity.class);
+				startActivity(intent);
+			}
+		});
+		return view;
 	}
 }
